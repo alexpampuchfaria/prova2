8f90ccc7efb9865ec43a31aae79c6c12ba9c6e4e
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-07-07 21:46:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-let timestotais = 0;
+timestotais = 0;
 let timesmax = 4;
-let mostraHTML = document.getElementById("mostraHTML")
+let mostraHTML = document.getElementById("mostraHTML");
 
 function cadastro(){
 
     if (timestotais >= timesmax) {
-      alert("Limite de 4 times atingido");
+      alert("Limite de 4 times atingido!");
       return;
     }
 
-    let nometime = prompt("Digite o nome do seu time: ")
-  
+    let nometime = prompt("Digite o nome do time: ");
+    
 
-    let aluno1 = prompt("Escreva o nome do aluno 1: ")
-    let aluno2 = prompt("Escreva o nome do aluno 2: ")
-    let aluno3 = prompt("Escreva o nome do aluno 3: ")
+    let aluno1 = prompt("Escreva o nome do jogado1: ");
+    let aluno2 = prompt("Escreva o nome do jogado2: ");
+    let aluno3 = prompt("Escreva o nome do jogador3: ");
 
-    let idade1 = Number(window.prompt(`Escreva a idade do ${aluno1}`))
-    let idade2 = Number(window.prompt(`Escreva a idade do ${aluno2}`))
-    let idade3 = Number(window.prompt(`Escreva a idade do ${aluno3}`))
+    let idade1 = Number(window.prompt(`Escreva a idade do ${aluno1}`));
+    let idade2 = Number(window.prompt(`Escreva a idade do ${aluno2}`));
+    let idade3 = Number(window.prompt(`Escreva a idade do ${aluno3}`));
 
     let media;
     
     media = (idade1 + idade2 + idade3) / 3;
+    
 
     if (media > 15) {
-        alert ("O seu time não poderá participar do interclasse pela média de idade!!")
-    }
-    
+        alert ("O seu time não poderá participar do interclasse pela média de idade!!");
+        alert("cadatre um time valído");
+      
+      }
+
+
     timestotais++;
 
     let lista = document.getElementById("listatimes");
-    let bloco = document.createElement("div")
+    let bloco = document.createElement("div");
 
-    mostraHTML.innerHTML += `<p class="textos">Time: <b>${nometime.toUpperCase()}</b>, Média de Idade: ${media}</p>`;
-   
-    jhonHTML.innerHTML += `<p class="textos">${aluno1}, ${idade1}</p>`;
-    frontHTML.innerHTML += `<p class="textos">${aluno2}, ${idade2}</p>`;
-    leSHTML.innerHTML += `<p class="textos">${aluno3}, ${idade3}</p>`;   
+    mostraHTML.innerHTML += `<h3> TIME: ${nometime.toUpperCase()}</h3>`;
+    mostraHTML.innerHTML += `<p>${aluno1}, ${idade1}</p>`;
+    mostraHTML.innerHTML += `<p>${aluno2}, ${idade2}</p>`;
+    mostraHTML.innerHTML += `<p>${aluno3}, ${idade3}</p>`; 
+    mostraHTML.innerHTML += `<p>Média de idade: ${media.toFixed(2)}</p><hr>`;
 }
